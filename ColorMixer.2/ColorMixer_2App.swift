b7f68346733595dd8d81444a48c6b1c5264a9be5
Armando <avaquerwks10@gmail.com>
2023-02-21 08:47:08 -0500

    Initial Commit

---
+++ ColorMixer.2/ColorMixer_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixer_2App.swift
+//  ColorMixer.2
+//
+//  Created by Armando Vaquer on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixer_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMixer.2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ColorMixer.2
+//
+//  Created by Armando Vaquer on 2/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
